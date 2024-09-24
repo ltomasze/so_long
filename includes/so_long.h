a144296d0bb263a8a74cf9c05b898c33162ae18f
Lukasz Tomaszewski <ltomasze@c2r5s3.42warsaw.pl>
2024-09-24 18:40:32 +0200

    close window with no leaks and remember add in so_long.h

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 14:19:55 by ltomasze          #+#    #+#             */
-/*   Updated: 2024/09/24 12:39:34 by ltomasze         ###   ########.fr       */
+/*   Updated: 2024/09/24 14:34:05 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,19 @@
 # include "../minilibx-linux/mlx.h"
 # include "../minilibx-linux/mlx_int.h"
 # include "get_next_line.h"
+
+// Struktura do przechowywania danych obrazu
+typedef struct s_data {
+    void    *img;
+    char    *addr;
+    int     bits_per_pixel;
+    int     line_length;
+    int     endian;
+    void    *mlx;  // Dodaj zmienną do przechowywania wskaźnika do MLX
+    void    *mlx_win; // Dodaj zmienną do przechowywania wskaźnika do okna
+}           t_data;
+
+void my_mlx_pixel_put(t_data *data, int x, int y, int color);
+int close_window(int keycode, t_data *data);
 
 #endif
--- src/so_long.c
+++ src/so_long.c
@@ -6,23 +6,60 @@
 /*   By: ltomasze <ltomasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 11:59:44 by ltomasze          #+#    #+#             */
-/*   Updated: 2024/09/24 12:15:51 by ltomasze         ###   ########.fr       */
+/*   Updated: 2024/09/24 17:58:00 by ltomasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-int main(void)
-{
-    void *mlx;
-    void *win;
+// Funkcja do rysowania piksela
+void my_mlx_pixel_put(t_data *data, int x, int y, int color) {
+    char *dst;
 
-    mlx = mlx_init(); // Inicjalizuj MLX
-    if (!mlx)
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+
+// Funkcja do zamykania okna za pomocą "X"
+int close_window_cross(t_data *data) {
+    mlx_destroy_image(data->mlx, data->img); // Zniszcz obraz
+    mlx_destroy_window(data->mlx, data->mlx_win); // Zniszcz okno
+    mlx_destroy_display(data->mlx);
+    free(data->mlx);
+    exit(0); // Zakończ program
+    return (0);
+}
+
+int close_window(int keycode, t_data *data) {
+    if (keycode == 65307) // ESC
+    {
+        close_window_cross(data); // Zakończ program, jak naciśnięto ESC
+    }
+    return (0);
+}
+
+int main(void) {
+    t_data img; // Tworzenie zmiennej typu t_data
+
+    img.mlx = mlx_init(); // Inicjalizuj MLX
+    if (!img.mlx) // Sprawdzenie błędów
         return (1);
-    win = mlx_new_window(mlx, 800, 600, "So Long!"); // Twórz nowe okno
-    if (!win)
+
+    img.mlx_win = mlx_new_window(img.mlx, 1920/2, 1080/2, "so_long"); // Tworzenie nowego okna
+    if (!img.mlx_win) // Sprawdzenie błędów
         return (1);
-    mlx_loop(mlx); // Rozpocznij pętlę MLX
+
+    img.img = mlx_new_image(img.mlx, 1920/2, 1080/2); // Tworzenie nowego obrazu
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian); // Pobieranie adresu danych obrazu
+
+    // Przykład rysowania piksela
+    my_mlx_pixel_put(&img, 5, 5, 0x00FF0000); // Rysowanie czerwonego piksela
+    mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 0, 0); // Wyświetlanie obrazu
+
+    // Rejestracja funkcji obsługującej zdarzenia klawiszy
+    mlx_key_hook(img.mlx_win, close_window, &img);
+    // Dodanie obsługi zamknięcia okna przez "X"
+    mlx_hook(img.mlx_win, 17, 0, close_window_cross, &img);
+    mlx_loop(img.mlx); // Rozpoczęcie pętli MLX
     return (0);
 }
